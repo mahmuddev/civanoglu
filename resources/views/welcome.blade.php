ef76572a45cb42e47345491c8752974d92b87bf3
mahmuddev <wafi.mahmud2022@gmail.com>
2021-12-25 07:26:46 +0600

    upload via git

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<x-guest-layout>
+    <div class="relative z-10 pt-48 pb-52 bg-cover bg-center" style="background-image: url('/img/hero-bg.jpg')">
+        <div class="absolute h-full w-full bg-black opacity-70 top-0 left-0 z-10"></div>
+        <div class="container relative z-20 text-white text-center text-2xl">
+            <h2 class="font-bold text-5xl mb-8">Guide Property - your guide <br/> to the primary real state market in Dubai</h2>
+            <p>The most convenient real state search portal in Dubai, start searching now!</p>
+        </div>
+    </div>
+    <div class="py-32 text-center">
+        <div class="container">
+            <h2 class=" font-bold text-4xl mb-6">Choose real estate by points, quickly <br/> and efficiently with the help of <span class=" underline">Smart search</span></h2>
+            <p class=" text-gray-500 text-2xl mb-10">Answer a few questions and get access to a personal selection of objects</p>
+            <a href="" class=" border-2 border-gray-700 rounded-2xl text-xl px-8 py-3 inline-block">Start the review</a>
+
+            <h2 class=" font-bold text-4xl mb-6 mt-20">Why should you choose a property <br/> with the help of our service?</h2>
+        </div>    
+    </div>
+    <div class="container py-14">
+        <h2 class="section-heading">Last added objects</h2>
+        <div class=" flex -mx-2 justify-between mt-10">
+            <div class=" flex-1 mx-2 shadow-lg relative rounded-md">
+                <a href="" class=" absolute left-3 w-9 h-9 leading-10 self-center text-base top-3 bg-black text-white bg-opacity-25 text-center hover:bg-yellow-500 hover:text-white duration-200 rounded-full text-base"><i class="fa fa-heart-o"></i></a>
+                <div class=" py-20 bg-center" style="..."></div>
+                <div class=" p-3">
+                    <h2 class=" text-base leading-10">Lorem ipsum dolor sit amet.</h2>
+                    <h3 class=" text-2xl py-3">AED 490,00</h3>
+                    <div class=" border-t-2">
+                        <ul class=" flex items-center my-4 -mx-1">
+                            <li class=" px-2 py-1 bg-blue-50 rounded-md text-xs mx-1 shadow-sm">7 bedrooms</li>
+                            <li class=" px-2 py-1 bg-blue-50 rounded-md text-xs mx-1 shadow-sm">5 bedrooms</li>
+                            <li class=" px-2 py-1 bg-blue-50 rounded-md text-xs mx-1 shadow-sm">1500 ft <sup>2</sup></li>
+                        </ul>
+                        <a href="" class=" btn w-full text-center">More details!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
