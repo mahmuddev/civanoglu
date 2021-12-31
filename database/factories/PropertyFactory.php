3d2b28740beb556c695b23cb807b032513b2a6a8
mahmuddev <wafi.mahmud2022@gmail.com>
2021-12-31 23:57:06 +0600

    8_PropertyFactory

---
+++ database/factories/PropertyFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PropertyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => '',
+            'featured_media_id' => '',
+            'location_id' => '',
+            'price' => '',
+            '' => '',
+            '' => '',
+            '' => '',
+            '' => '',
+            '' => '',
+            '' => '',
+            '' => '',
+            '' => '',
+            '' => '',
+            '' => '',
+            '' => '',
+        ];
+    }
+}
