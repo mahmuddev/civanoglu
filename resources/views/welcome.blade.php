5c0231c4af630eb14b47790caa8f7c558965acc7
mahmuddev <wafi.mahmud2022@gmail.com>
2022-01-05 13:17:55 +0600

    11_10:15_search_option

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,30 +11,30 @@
     </div>
     <!-- Search From Area -->
     <div class="rounded-lg container bg-white p-4 relative -m-10 z-30 shadow-lg">
-        <form action="" method="GET" class="flex justify-between">
+        <form action="{{ route('properties') }}" method="GET" class="flex justify-between">
             <div class="flex w-7/12 justify-between">
                 <div class="flex flex-col mx-3">
-                    <select name="sale" class="border-0">
-                        <option value="">Rent or Sale</option>
-                        <option value="1">Rent</option>
-                        <option value="2">Sale</option>
+                    <select name="sale" class="border-0 focus:ring-0" required>
+                        <option value="">Buy or Rent</option>
+                        <option value="1">Buy</option>
+                        <option value="0">Rent</option>
                     </select>
                 </div>
                 <div class="py-3 self-center border-gray-500 border-2"></div>
                 <div class="flex flex-col mx-3">
-                    <select name="type" class="border-0">
+                    <select name="type" class="border-0 focus:ring-0" required>
                         <option value="">Type</option>
-                        <option value="land">Land</option>
-                        <option value="appartment">Appartment</option>
-                        <option value="villa">Villa</option>
+                        <option value="0">Land</option>
+                        <option value="1">Appartment</option>
+                        <option value="2">Villa</option>
                     </select>
                 </div>
                 <div class="py-3 self-center border-gray-500 border-2"></div>
                 <div class="flex flex-col mx-3">
-                    <select name="price" class="border-0">
+                    <select name="price" class="border-0 focus:ring-0">
                         <option value="">Price</option>
                         <option value="100000">0 - 1,00,000</option>
-                        <option value="200000">2,00,000 - 3,00,000</option>
+                        <option value="200000">1,00,000 - 2,00,000</option>
                         <option value="300000">3,00,000 - 4,00,000</option>
                         <option value="400000">4,00,000 - 5,00,000</option>
                         <option value="500000">5,00,000 <span class="font-serif">+</span></option>
@@ -42,11 +42,13 @@
                 </div>
                 <div class="py-3 self-center border-gray-500 border-2"></div>
                 <div class="flex flex-col mx-3">
-                    <select name="bedrooms" class="border-0">
+                    <select name="bedrooms" class="border-0 focus:ring-0">
                         <option value="">Bedrooms</option>
-                        
-                        <option value=""></option>
-                    
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
                     </select>
                 </div>
             </div>
